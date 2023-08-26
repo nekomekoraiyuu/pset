34c9c76f342ef09883d4db8d116e1e11e36948f3
nekomekoraiyuu <raini4645@gmail.com>
2023-08-26 18:32:12 +0600

    install: Add basic stuff [1/3] (W.I.P)

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # PSET : INIT
+set -x
 # ----- MISC
+CURSPATH="$(realpath $0)" && CURSPATH="$(dirname "$CURSPATH")"
 CLR_RESET='\e[0m'
 CLR_BOLD='\e[1m'
 # ----- VARS
@@ -9,10 +11,26 @@ temp_current_shell_proc=$(ps -o comm= $PPID)
 compatible_shell=(
 	"bash"
 )
+# Required dependencies
+required_deps=(
+	"fzf"
+	"git"
+)
 # ----- FUNCTIONS
 # 
 parse_arg () {
 	:
+}
+# A function to check if installing from cloned git repo or Online
+check_inst () {
+	if [ "$(git -C "$CURSPATH" rev-parse 2>/dev/null; echo "$?")" -eq 0 ];
+		then
+			# 0 for local git repo install
+			echo "0" && return
+		else
+			# 1 for online git repo
+			echo "1" && return
+	fi
 }
 # ------ MAIN
 # Relaunch script with required args
@@ -33,6 +51,32 @@ if [ ! "${1}" == "--relaunch-args" ];
 			then
 				echo -e ">Current Shell ${current_shell_proc} isn't in the shell script compatibility list!\n>Please make a github issue to add your current shell to the list!"
 				exit 1
+		fi
+		# Check installation type
+		if [ "$(check_inst)" == "0" ];
+			then
+				echo -e ">Installing from local git repo at: ${CURSPATH}"
+				install_type="local"
+			else
+				echo -e ">Installing script from online git repo"
+				install_type="online"
+		fi
+		# Check for dependencies
+		for i in ${required_deps[@]}
+			do
+				if [ ! "$(command -v "$i")" ];
+					then
+						echo -e "!>$i Is not installed!"
+						exit 2
+				fi
+			done
+		# Now Actually "install" the script
+		if [ "$install_type" == "local" ];
+			then
+				cp $CURSPATH/pset.sh /bin/pset
+			else
+			# To do
+				:
 		fi
 fi
 
