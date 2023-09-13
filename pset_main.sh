a68dce13469951c22a5fbbaa5cdd9c9f2b292aba
nekomekoraiyuu <raini4645@gmail.com>
2023-09-13 19:11:30 +0600

    main: tinkering with functions (w.i.p)
    * pretty much a big brain when you use the same regex twice LMAO
    * imagine using nested case

--- pset_main.sh
+++ pset_main.sh
@@ -2,6 +2,8 @@
 # -- variables
 # we need a ifs backup!
 IFSBAK="${IFS}"
+# IFS is a newline indeed
+IFS=$'\n'
 CUR_PROC=$(ps -o comm= $PPID)
 SUPPORTED_SHELLS=(
 "bash"
@@ -12,10 +14,55 @@ confdirs=(
 	"$PSETCONF/process_configs"
 	"$PSETCONF/process_configs/$CUR_PROC"
 )
+setdir="${confdirs[@]:2:3}"
+procdir=(
+"$setdir/{current_vars,current_cmds,temp_restore}"
+)
 # -- Functions
-:
+printout () {
+case ${1} in
+	-e | --error)
+		echo -e "${2}"
+		exit "${3:-"9"}"
+	*)
+		echo "${FUNCNAME[0]}: invalid argument: ${1}!"
+		exit 6
+esac
+}
+# Empty arg checking function
+check_arg () {
+	if [ ${1:-doesntexist} ];
+		then
+			if grep -qoP "${2}" <<< "${1}";
+				then
+					echo 0
+				else
+				# 2 is not match
+					echo 2
+			fi
+		else
+		# Arg empty
+			echo 1
+	fi
+}
+check_dir () {
+	# Create some files if they dont exist
+	for i in "${procdir[@]}"
+		do
+			if [ -f "$i" ];
+				then
+					(cd "$setdir" && touch "${i##*/}")
+			fi
+		done
+}
 pset_set () {
-:
+		# Uh need to remove some stuff first
+		local varname=$(grep -op '^[\w]+=.*' <<< "${1}")
+		if grep -qo "${varname}" < "${procdir[@]:0:1}";
+			then
+				sed -i "s/${varname}/${1}" "${procdir[@]:0:1}" 
+			else
+		fi
 }
 # check if current shell is in supported shell variables
 if [ ! "${SUPPORTED_SHELLS[@]}" =~ \b$CUR_PROC\b ];
@@ -29,7 +76,7 @@ for f in "${confdirs[@]}"
 	do
 		if [ ! -d "$f" ];
 			then
-				mkdir -p "-f"
+				mkdir -p "$f"
 		fi
 	done
 # -- MAIN
@@ -40,7 +87,15 @@ if [ "$#" -ge 1 ];
 			do
 				case $i in
 					-x | --set)
-						# place holder func here
+						# Argument check
+						case "$(check_arg "${2}" '^[\w]+=.*')" in
+							1)
+								printout -e "${0##*/}: ${1}: Argument empty\!"
+							;;
+							2)
+								printout -e "${0##*/}: ${1}: Invalid argument\!"
+							;;
+						esac
 						shift 1
 				esac
 			done
